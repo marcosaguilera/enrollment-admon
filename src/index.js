0498bdad2e88d34ae6cd9d8464bf04a001f96aa1
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-07-03 12:23:17 -0500

    index js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+
+//Assets
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 // Router
 import AppRoutes from './routes'
