da1fa65c80eea44be03e0e0ecdab9e89046fe8b3
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-07-06 16:52:43 -0500

    Login

--- src/Components/Modulars/Login/Login.js
+++ src/Components/Modulars/Login/Login.js
@@ -2,6 +2,9 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
+import './Login.css';
+import './signin.css';
+
 class Login extends Component {
   constructor(props){
     super(props);
@@ -17,14 +20,34 @@ class Login extends Component {
   render() {
     return (
       <div className="Login">
-        <h1>This the login window</h1>
-        <Container className="text-center">
+
+        <Container style={{ marginTop: '30%' }}>
             <Row>
-              <Col>Main container</Col>
+              <Col xs="3"></Col>
+              <Col xs="6">
+                
+                <form className="form-signin">
+                    <img className="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72" />
+                    <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+                    <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+                    <div className="checkbox mb-3">
+                      <label>
+                        <input type="checkbox" value="remember-me"/> Remember me
+                      </label>
+                    </div>
+                    <button className="btn btn-lg btn-primary btn-block" 
+                            type="submit"
+                            onClick={() => this.nextPath()}>Google Account Sign-in</button>
+                    <p className="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+                </form>
+
+              </Col>
+              <Col xs="3"></Col>
             </Row>
-            <button type="button"
-                    className="btn btn-primary" 
-                    onClick={() => this.nextPath()}>Primary</button>
+            
         </Container>
       </div>
     );
