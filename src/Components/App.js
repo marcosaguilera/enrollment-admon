e412a2fb5d71961408f8b40e6b49010e88c39b1c
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-07-06 16:52:34 -0500

    App changed component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import './assets/css/App.css';
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
-
 //import PropType from 'prop-types';
 
 //Components declaration
-//import Login from './Modulars/Login/Login'
-import Main from './Modulars/Main/Main'
+import Login from './Modulars/Login/Login'
+//import Main from './Modulars/Main/Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Main />
+          <Login />
       </div>
     );
   }
