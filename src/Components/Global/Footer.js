3b31c670fa9952a8882b49deba2a92fe1a7cffe6
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-07-12 15:17:53 -0500

    Logo at login window

--- src/Components/Global/Footer.js
+++ src/Components/Global/Footer.js
@@ -6,6 +6,27 @@ import PropTypes from 'prop-types';
 import '../Global/assets/css/Footer.css';
 
 class Footer extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      year : 1999
+    }
+  }
+
+  componentDidMount(){
+    var now = new Date();
+    //console.log(now);
+    var yearNow = now.getFullYear();
+    //console.log(yearNow);
+    this.setState({
+      year : yearNow
+    }, () => {
+      //console.log("year updated!");
+    })
+  }
+
   static propTypes = {
     copyright: PropTypes.string
   };
@@ -13,13 +34,16 @@ class Footer extends Component {
   render() {
     const { copyright = '&copy; React 2018' } = this.props
     return (
-      <div className="Footer bg-light text-dark">
-        <footer className="footer">
-            <div className="container">
-              <p id="pfooter">{copyright} | <a href="https://rochester.edu.co/politica-proteccion-datos/" rel="noopener noreferrer" target="_blank">Protección de Datos</a> </p> 
+      <div className="bg-light text-dark">
+        {/* BEGIN: Sticky Footer */}
+            <div id="footer_container">
+                <div id="footer">
+                  <p id="pfooter">{copyright} {this.state.year} | <a href="https://rochester.edu.co/politica-proteccion-datos/" rel="noopener noreferrer" target="_blank">Protección de Datos</a> </p> 
+                </div>
             </div>
-        </footer>
-      </div>
+        {/* END: Sticky Footer */}    
+    </div>
+    
     );
   }
 }
