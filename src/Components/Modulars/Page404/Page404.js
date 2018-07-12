34f15324f88cc7d9f60ebb2f803f254aae22b319
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-07-12 16:56:11 -0500

    Page404 nice!
    I’ve improve 404 page. It seems nice.

--- src/Components/Modulars/Page404/Page404.js
+++ src/Components/Modulars/Page404/Page404.js
@@ -2,10 +2,33 @@
 import React, { Component } from 'react';
 
 class Page404 extends Component {
+
+  constructor(props){
+    super(props);
+    
+    this.nextPath      = this.nextPath.bind(this);
+  }
+
+  nextPath = () => {
+      console.log(this.props);
+      this.props.history.push('/login');
+  }
+
   render() {
     return (
       <div className="Page404">
-        <h1>Page404</h1>
+        <div className="jumbotron jumbotron-fluid bg-white text-dark">
+          <div>
+            <center>
+              <h1 className="display-2">¯\_(ツ)_/¯</h1>
+              <h1>Page404</h1>
+              <h3 className="display-5 py-3">Ups! something went wrong. Let me help you.</h3>
+              <button type="button" 
+                      className="btn btn-lg btn-outline-primary"
+                      onClick={this.nextPath}>¡Yeah! Take out here.</button>
+            </center>
+          </div>
+        </div>
       </div>
     );
   }
